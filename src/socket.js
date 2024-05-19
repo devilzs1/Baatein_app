193306548f10642232e2a195211d34beb723a490
Mohammad Adil <mohammadadil3292@gmail.com>
2024-05-19 22:06:38 +0530

    updated route

--- src/socket.js
+++ src/socket.js
@@ -1,15 +1,15 @@
 import io from "socket.io-client";
 let socket;
 
-// const connectSocket = (user_id) => {
-//   socket = io("https://baatein-app-backend.vercel.app/", {
-//     query: `user_id=${user_id}`,
-//   });
-// };
 const connectSocket = (user_id) => {
-  socket = io("http://localhost:3000", {
+  socket = io("https://baatein-app-backend.vercel.app", {
     query: `user_id=${user_id}`,
   });
 };
+// const connectSocket = (user_id) => {
+//   socket = io("http://localhost:3000", {
+//     query: `user_id=${user_id}`,
+//   });
+// };
 
 export { socket, connectSocket };
