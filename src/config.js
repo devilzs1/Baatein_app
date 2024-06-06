c91f1d2be5c1e187472dff5b597fbe4cf68b75ff
Mohammad Adil <97207850+devilzs1@users.noreply.github.com>
2024-06-06 14:26:14 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "https://baatein-app-backend.vercel.app";
+export const BASE_URL = "https://baatein-app-backend.onrender.com/";
 // export const BASE_URL = "http://localhost:3000";
 
 export const defaultSettings = {
